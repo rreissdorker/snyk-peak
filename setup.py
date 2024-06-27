f3586134e01ac5179b80d219fd1e4200958191bd
Rreiss'"sHackerOne/s <72650266+rreissdorker@users.noreply.github.com>
2024-06-27 13:41:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='sp',
-    version='1.0.0',
+    name='sp\'"><s>dd',
+    version='1.0.0<s>sd',
     description='A mali Python package',
     long_description='This package contains a mali submodule configuration designed to exploit CVE-2020-11008.',
     long_description_content_type='text/markdown',
