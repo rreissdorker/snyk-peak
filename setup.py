3975371196c8d5fcdb7552253ba52a7fa7c31955
Rreiss'"sHackerOne/s <72650266+rreissdorker@users.noreply.github.com>
2024-07-01 01:16:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def sdesc():
 
 setup(
     name='my_gh_package',
-    version='1.5.3',
+    version='1.5.4',
     description=sdesc(),
     long_description='Some random long description',
     long_description_content_type='text/markdown',
